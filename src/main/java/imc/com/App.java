92ddcdd5fb3097208381bc07b2e5dbed23a3e37a
ivannnnnn115 <yehtetag1152005@gmail.com>
2025-10-01 13:43:23 +0630

    lab3 semi-final2

--- src/main/java/imc/com/App.java
+++ src/main/java/imc/com/App.java
@@ -3,7 +3,15 @@ import java.sql.*;
 
 public class App
 {
-    public static void main(String[] args)
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
     {
         try
         {
@@ -16,9 +24,7 @@ public class App
             System.exit(-1);
         }
 
-        // Connection to the database
-        Connection con = null;
-        int retries = 100;
+        int retries = 15;
         for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
@@ -27,12 +33,8 @@ public class App
                 // Wait a bit for db to start
                 Thread.sleep(10000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false&allowPublicKeyRetrieval=true",
-                        "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?allowPublicKeyRetrieval=true&useSSL=false&allowPublicKeyRetrieval=true", "root", "example");
                 System.out.println("Successfully connected");
-                // Wait a bit
-//                Thread.sleep(10000);
-//                // Exit for loop
                 break;
             }
             catch (SQLException sqle)
@@ -45,7 +47,13 @@ public class App
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
 
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
         if (con != null)
         {
             try
@@ -58,5 +66,17 @@ public class App
                 System.out.println("Error closing connection to database");
             }
         }
+    }
+
+    public static void main(String[] args)
+    {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        // Disconnect from database
+        a.disconnect();
     }
 }
